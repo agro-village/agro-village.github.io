fca54c223f4223e3b63822376be728b8fb4bb4f5
AhmedE404 <AhmedE404@users.noreply.github.com>
2026-09-02 10:11:11 +0000

    deploy: staging 4d7883930833128597d2745fed6f896eb5917b69 agro-village/agro-village@4d7883930833128597d2745fed6f896eb5917b69

---
+++ _next/static/chunks/4286g2q25plr7.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,1727,e=>{"use strict";var t=e.i(71645),i=e.i(18566),u=e.i(34231);e.s(["default",0,function(){let e=(0,i.useRouter)();return(0,t.useEffect)(()=>{u.authService.isAuthenticated()?e.replace("/admin/dashboard"):e.replace("/admin/login")},[e]),null}])}]);
